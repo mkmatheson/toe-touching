e33de676753cdb70a188a7e61d139ddaf2fadc31
mkmatheson <mckaymatheson@gmail.com>
2025-03-02 10:22:54 -0800

    add react component for use in parent website

---
+++ toe-touching.tsx
@@ -0,0 +1,85 @@
+import {
+  currentYear,
+  completedDays,
+  months,
+  currentDaysNoScroll,
+  maxDaysNoScroll
+} from './const';
+import './toe-touching.css';
+
+const Day = ({ day, month }: { day: number; month: number }) => {
+  const today = new Date();
+  const classes = ['day'];
+  if (
+    today.getFullYear() === currentYear &&
+    today.getMonth() === month &&
+    today.getDate() === day
+  ) {
+    classes.push('today');
+  }
+  if (
+    completedDays[month + 1].length > 0 &&
+    completedDays[month + 1].includes(day)
+  ) {
+    classes.push('completed');
+  }
+  return <div className={classes.join(' ')}>{day}</div>;
+};
+
+const Month = ({
+  monthName,
+  monthIdx
+}: {
+  monthName: string;
+  monthIdx: number;
+}) => {
+  const firstDayOfMonth = new Date(currentYear, monthIdx, 1);
+  const lastDayOfMonth = new Date(currentYear, monthIdx + 1, 0);
+  const numDaysInMonth = lastDayOfMonth.getDate();
+  const firstDayOfWeek = firstDayOfMonth.getDay();
+
+  return (
+    <div className="month">
+      <h3>{monthName}</h3>
+      <div className="days">
+        {Array.from({ length: firstDayOfWeek }).map((_) => (
+          <div className="day" />
+        ))}
+        {Array.from({ length: numDaysInMonth }).map((_, day) => (
+          <Day day={day + 1} month={monthIdx} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Calendar = () => {
+  return (
+    <div id="calendar">
+      {months.map((monthName, idx) => (
+        <Month monthName={monthName} monthIdx={idx} />
+      ))}
+    </div>
+  );
+};
+
+const ToeTouching = () => {
+  return (
+    <div>
+      <div>
+        <h1>Did McKay Try Touching His Toes Every Day For a Full Year?</h1>
+        <h3 className="subtitle">
+          A red slash through the date means McKay tried touching his toes that
+          day
+        </h3>
+
+        <h3 id="streak"></h3>
+        <h3>Consecutive days without doomscrolling: {currentDaysNoScroll}</h3>
+        <h3>Max no-scroll streak: {maxDaysNoScroll}</h3>
+      </div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default ToeTouching;
