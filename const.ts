b2f2cd5e13280f645c81d3cb0032287b2866665a
mkmatheson <48038833+mkmatheson@users.noreply.github.com>
2025-03-29 10:45:07 -0700

    day 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88

--- const.ts
+++ const.ts
@@ -37,7 +37,7 @@ export const completedDays: { [key: number]: (number | null)[] } = {
     27,
     28
   ],
-  3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 12, null, 14, null, 16],
+  3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 12, null, 14, null, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, null, 29],
   4: [],
   5: [],
   6: [],
