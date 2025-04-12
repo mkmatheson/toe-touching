a0c598f40aaeba5676cfd8018449f5c27220df8b
mkmatheson <48038833+mkmatheson@users.noreply.github.com>
2025-04-12 09:43:54 -0600

    a bunch of days

--- const.ts
+++ const.ts
@@ -1,6 +1,6 @@
 export const maxDaysNoScroll = 23;
 
-export const currentDaysNoScroll = 0;
+export const currentDaysNoScroll = 1;
 
 export const completedDays: { [key: number]: (number | null)[] } = {
   1: [
@@ -37,8 +37,8 @@ export const completedDays: { [key: number]: (number | null)[] } = {
     27,
     28
   ],
-  3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 12, null, 14, null, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, null, 29],
-  4: [],
+  3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 12, null, 14, null, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, null, 29, null, 31],
+  4: [1,2,3,4,5,null,7,8,null,10,11,12],
   5: [],
   6: [],
   7: [],
